c58406fec589f2875f49ac91a0366a0dca02e3cb
jhonf19 <zjhon2018@gmail.com>
2019-11-08 13:25:30 -0500

    new driver mongo 4

--- src/database.js
+++ src/database.js
@@ -1,11 +1,8 @@
 const mongoose = require('mongoose');
 
 // mongoose.connect('mongodb://localhost/note-app', {
-mongoose.set('useFindAndModify', false);
 mongoose.connect('mongodb+srv://note_app:ormZFJ56a1WOMUQh@cluster0-ambcv.mongodb.net/note-app?retryWrites=true&w=majority', {
-  useUnifiedTopology: true,
-  useCreateIndex: true,
-  useNewUrlParser: true
+    useNewUrlParser: true
 })
   .then(db => console.log('DB is ok'))
   .catch(err => console.error(err));
