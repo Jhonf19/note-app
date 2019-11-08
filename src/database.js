cdc8dd10feaeb4bb8e44b3153e3dc595d2bd31b8
jhonf19 <zjhon2018@gmail.com>
2019-11-08 13:10:41 -0500

    new driver mongo 3

--- src/database.js
+++ src/database.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 // mongoose.connect('mongodb://localhost/note-app', {
 mongoose.set('useFindAndModify', false);
-mongoose.connect('mongodb+srv://note_app:ormZFJ56a1WOMUQh@cluster0-ambcv.mongodb.net/test?retryWrites=true&w=majority', {
+mongoose.connect('mongodb+srv://note_app:ormZFJ56a1WOMUQh@cluster0-ambcv.mongodb.net/note-app?retryWrites=true&w=majority', {
   useUnifiedTopology: true,
   useCreateIndex: true,
   useNewUrlParser: true
