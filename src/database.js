d233d7766317fdb8f2fdbfb1d387417c1524dcc6
jhonf19 <zjhon2018@gmail.com>
2019-11-08 12:43:10 -0500

    Mongo DR Commpas

--- src/database.js
+++ src/database.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 mongoose.set('useFindAndModify', false);
 // mongoose.connect('mongodb://localhost/note-app', {
-mongoose.connect('mongodb+srv://note_app:ormZFJ56a1WOMUQh@cluster0-ambcv.mongodb.net/test?retryWrites=true&w=majority', {
+mongoose.connect('mongodb+srv://note_app:ormZFJ56a1WOMUQh@cluster0-ambcv.mongodb.net/test', {
   useUnifiedTopology: true,
   useCreateIndex: true,
   useNewUrlParser: true
